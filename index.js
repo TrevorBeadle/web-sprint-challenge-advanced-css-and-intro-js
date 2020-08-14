ce5e4eb2d263de42fd701c626ab1425253e60f44
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-08-14 11:50:40 -0500

    finished MVP tasks for JS

--- index.js
+++ index.js
@@ -207,8 +207,8 @@ const artists = [
 
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
-
-
+console.log(artists[0].name);
+console.log(artists[2].bio);
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
@@ -223,19 +223,23 @@ const artists = [
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(array, index) {
-    /* code here */
+    return `The artist at index ${index} is ${array[index].name}`
   }
+  console.log(getArtistByIndex(artists, 0));  
   
   /**
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/* Code here */){
-
-  /* Code here */
-
+function get20s(arr){
+  for(let i = 0; i < arr.length; i++){
+    if((arr[i]['years'].split(' ')[0] >= 1900) && (arr[i]['years'].split(' ')[2] < 2000)){
+      return(arr[i]);
+    }
+  }
 }
+console.log(get20s(artists));
 
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
@@ -248,10 +252,11 @@ function get20s(/* Code here */){
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*code here*/) {
-    /* code here */
+function removeArtist(arr, index){
+    arr.splice(arr[index], 1);
+    console.log(arr.length);
   }
-  
+  removeArtist(artists, 0);
  
 
 /**
@@ -267,11 +272,12 @@ bio: Add 1-2 sentences (or use lorem ipsum)
 
 At the end, this function should return the new array with information added"*/
 
-function addArtist(/* Code here */){
-
-    /* Code here */
-
-  }
+function addArtist(arr, id, name, years, genre, nationality, bio){
+  const newObject = {id, name, years, genre, nationality, bio};
+  arr.push(newObject);
+  return arr;
+}
+console.log(addArtist(artists, 20, 'Trevor', '1996 - current day', 'Web Design', 'Filipino', "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."));
 
 /* Task 7: Create a function called lotsOfArt() that takes one argument: 
 
@@ -281,11 +287,16 @@ and returns an array with names of artists who painted more than 100 paintings.
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ..."Albrecht Dürer"]*/
 
-function lotsOfArt(/* Code here */){
-
-  /* Code here */
-
+function lotsOfArt(arr){
+  let newArr = [];
+  for (i = 0; i < arr.length; i++){
+    if (arr[i].paintings > 100){
+      newArr.push(arr[i].name);
+    }
+  }
+  return newArr;
 }
+console.log(lotsOfArt(artists));
 
 
 
